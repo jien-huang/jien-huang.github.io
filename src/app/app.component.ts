e47ac8d61d824523f9c432033cfbc02a7a78e6f9
jien.huang <huangjien@gmail.com>
2019-12-04 16:46:00 +0000

    add key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,11 @@ import { GraphQLClient } from 'graphql-request';
 
 const endpoint = 'https://api.github.com/graphql';
 
-const A= "1234567890"
-const B= "abcdef"
+const key= atob("YzA4Njc3N2VmZjVkMmJlN2RlYWE4ZDk3MzRhY2I4N2Y2MmIwMDg0OQo=")
 
 const graphQLClient = new GraphQLClient(endpoint, {
   headers: {
-    authorization: `bearer ${A}${B}`,
+    authorization: `bearer ${key}`,
     'X-Requested-With': 'XMLHttpRequest',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
   },
@@ -31,4 +30,5 @@ const Http = (query = "", variables = {}, alive = false) => new Promise((resolve
 export class AppComponent {
   title = 'www.automation-test.com';
   intro = '<h1>No. 1 Rule</h1>\n<ol>\n<li>Keep the unit test coverage rate above 90% .</li>\n<li>Refactor it at least once per week.</li>\n<li>Make it workable, then beautify it.</li>\n</ol>';
+  about = '<h1>No. 1 Rule</h1>\n<ol>\n<li>Keep the unit test coverage rate above 90% .</li>\n<li>Refactor it at least once per week.</li>\n<li>Make it workable, then beautify it.</li>\n</ol>';
 }
