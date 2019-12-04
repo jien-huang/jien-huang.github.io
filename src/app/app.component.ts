aaf7d048e79b4f6e2aec31955d7928848185f8e0
huangjien <pangyazhi@gmail.com>
2019-12-04 23:34:18 +0000

    use github issue as section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { GraphQLClient } from 'graphql-request';
 
 const endpoint = 'https://api.github.com/graphql';
 
-const key= atob("YzA4Njc3N2VmZjVkMmJlN2RlYWE4ZDk3MzRhY2I4N2Y2MmIwMDg0OQo=")
+const key = atob("YzA4Njc3N2VmZjVkMmJlN2RlYWE4ZDk3MzRhY2I4N2Y2MmIwMDg0OQo=")
 
 const graphQLClient = new GraphQLClient(endpoint, {
   headers: {
@@ -29,6 +29,37 @@ const Http = (query = "", variables = {}, alive = false) => new Promise((resolve
 })
 export class AppComponent {
   title = 'www.automation-test.com';
-  intro = '<h1>No. 1 Rule</h1>\n<ol>\n<li>Keep the unit test coverage rate above 90% .</li>\n<li>Refactor it at least once per week.</li>\n<li>Make it workable, then beautify it.</li>\n</ol>';
-  about = '<h1>No. 1 Rule</h1>\n<ol>\n<li>Keep the unit test coverage rate above 90% .</li>\n<li>Refactor it at least once per week.</li>\n<li>Make it workable, then beautify it.</li>\n</ol>';
+  intro = '';
+  about = '';
+  instance = '';
+  script = '';
+
+  query_str = `{
+    search(query: "repo:jien-huang/jien-huang.github.io label:internal" type: ISSUE, first: 100) {
+      nodes {
+        ... on Issue {
+          number
+          title
+          bodyHTML
+        }
+      }
+    }
+  }`
+
+  ngOnInit() {
+    Http(this.query_str).then(res => {
+      var nodes = res['search']['nodes'];
+      this.about = this.getHtmlByTitle(nodes, 'About');
+      this.intro = this.getHtmlByTitle(nodes, 'Introduction');
+      this.instance = this.getHtmlByTitle(nodes, 'Instance');
+      this.script = this.getHtmlByTitle(nodes, 'Script');
+    })
+  }
+
+  private getHtmlByTitle(nodes: any, title: string): string {
+    var node = nodes.find(el => el.title === title);
+    if (node) {
+      return node.bodyHTML;
+    }
+  }
 }
